c5f4b356c265a9b79afea1225ee40e6a113c237f
Bryce Guinta <azrathud@gmail.com>
2013-02-03 19:36:38 -0700

    amend try statement to catch all exceptions and simply print the error and then retry to connection

--- recieve-tcp.py
+++ recieve-tcp.py
@@ -9,11 +9,10 @@ import sys
 #   not be opened to outside IPs
 TCP_IP = ''
 TCP_PORT  = 1180
-BUFFER_SIZE = 20 # Norally 1024, but we want a fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT)) # still needs to be a tupple
-s.listen(1)
+s.listen(1) # Only l isten for one connection max
 
 while True:
     conn, addr = s.accept() # Accept any connections. 
@@ -25,13 +24,12 @@ while True:
             # Send test data
             conn.send("foo\n")
             conn.send("bar\n")
-        except socket.error, e: 
+        except: 
             # If the client closes the connection, don't quit and
             #   look for a new one
-            print "socket error({0}): {1}. Listening for new connection" \
-                    .format(e.errno, e.strerror)
+            traceback.print_exc()
             conn.close()
             break
-        time.sleep(1) # Do I need a wait?
+        time.sleep(1) # Do I need a wait? -- rapid sucession of sends else
 
 conn.close()
